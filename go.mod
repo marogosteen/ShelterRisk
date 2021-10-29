0473322bcc83212c835c80812ac5e24606710fe7
sugimoto <sugi3588fkcs@outlook.jp>
2021-10-29 12:50:39 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example/OSURisk
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("\nrun...\n\n")
+
+	make_kuku()
+	//fizz_buzz()
+	// zihanki(1000)
+	//suisoku_game()
+	//hairetsu()
+} 
