e59ef2617a87b2b9b2fb6e2d3d8a9588d7b69d55
Masahiro noma <macharo.0316@gmail.com>
2021-11-08 12:31:25 +0900

    peopleとsimulationのメソッド触った

--- people/people.go
+++ people/people.go
@@ -4,12 +4,25 @@ type People struct {
 	PeopleList []Person
 }
 
-func NewPeople(peopleCount int) *People{
+func NewPeople(peopleCount int) *People {
 	people := People{
 		PeopleList: make([]Person, peopleCount),
 	}
-	for i:=0; i<100; i++{
+	for i := 0; i < 100; i++ {
 		people.PeopleList[i] = *NewPerson(i)
 	}
 	return &people
+}
+
+func (p *People) Move() {
+	for _, person := range p.PeopleList {
+		person.Move()
+	}
+}
+
+// TODO IncubationからInfectionになるプログラム
+func (p *People) InfectionTest() {
+	for _, person := range p.PeopleList {
+		person.InfectionTest()
+	}
 }
